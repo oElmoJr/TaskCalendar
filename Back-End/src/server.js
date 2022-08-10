6bd3f4083c914ce1d155e9407cdce88e23bd3412
ElmoJr <elmojuniorduarte@gmail.com>
2022-08-10 18:34:23 -0300

    create server.js

---
+++ Back-End/src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+const port = 3333;
+
+app.use(routes);
+
+app.listen(port, () => {
+    console.log(`⚡ Backend started at http://localhost:${port}`);
+});
