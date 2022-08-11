48ee1ab4b0121cfe231405a0483f0bfc1b294335
ElmoJr <elmojuniorduarte@gmail.com>
2022-08-10 22:47:54 -0300

    create database connection

---
+++ Back-End/src/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+function connectToDatabase() { 
+    mongoose.connect(
+        process.env.DATABASE_URL, 
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    );
+    
+    const db = mongoose.connection;
+    db.on("error", (error) => console.log(error))
+    db.once("open", () => console.log("📦 Connected to the database!"))
+}
+
+module.exports = connectToDatabase
--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const routes = require("./routes");
+const connectToDatabase = require("./database");
+
+connectToDatabase()
 
 const app = express();
 const port = 3333;
