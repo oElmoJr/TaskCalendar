157b9c30d8cc81438cffd12b0706c82ba4dd2603
ElmoJr <elmojuniorduarte@gmail.com>
2022-08-11 17:12:15 -0300

    create a id validation middleware

---
+++ Back-End/src/middlewares/TaskMiddlewares.js
@@ -0,0 +1,26 @@
+const { validate: isUuid } = require("uuid");
+const Task = require("../models/Task")
+
+module.exports = {
+    async validateId(request, response, next) {
+        const { id } = request.params;
+
+        if (!isUuid(id)) {
+            return response.status(400).json({ error: "Invalid ID." })
+        }
+
+        try {
+            const task = await Task.findById(id);
+            response.Task = task;
+
+            if (!task) {
+                return response.status(404).json({ error: "Task not found."});
+            }
+
+        } catch (error) {
+            return response.status(500).json({ error: error.message })
+        }
+
+        next();
+    }
+}
