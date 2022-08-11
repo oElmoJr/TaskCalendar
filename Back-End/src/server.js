8beb0301986850448cd2d4e7a0a0ecdf2821d4a2
ElmoJr <elmojuniorduarte@gmail.com>
2022-08-11 17:16:15 -0300

    add cors to server.js

--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors")
 const routes = require("./routes");
 const connectToDatabase = require("./database");
 
@@ -9,6 +10,7 @@ connectToDatabase()
 const app = express();
 const port = 3333;
 
+app.use(cors())
 app.use(express.json())
 app.use(routes);
 
