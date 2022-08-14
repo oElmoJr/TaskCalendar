7112ef6faf897edf79ac653d1a424223ef2795d9
ElmoJr <elmojuniorduarte@gmail.com>
2022-08-14 19:52:23 -0300

    create Calendar component

---
+++ Front-End/web/src/components/Calendar/styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    
+    padding: 0 100px;
+    width: 100%;
+
+    .fc-button-primary { // color of calendar header buttons
+        background-color: #363BC4 !important;
+        border-color: #363BC4 !important;
+
+        &.fc-button-active, // active button
+        &:hover {
+            background-color: #181842 !important;
+            border-color: #181842 !important;
+        }
+    }
+
+    .fc-daygrid-day, .fc-col-header-cell, .fc-scrollgrid { // border of days on month view
+        border: 1px solid #A5A5A5 !important;
+    }
+
+    .fc-day-today { // today
+        background-color: #CACBE1 !important;
+    }
+
+    .fc-day-other { // past-month days
+        background-color: #CFCFCF;
+    }
+
+    .fc-toolbar-title { // calendar title
+        font-weight: 500;
+    }
+`;
