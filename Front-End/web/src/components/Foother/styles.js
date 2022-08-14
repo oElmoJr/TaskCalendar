754f38ae0b2e4a0fef9195ca31c9188d9d7acaf2
ElmoJr <elmojuniorduarte@gmail.com>
2022-08-14 19:51:39 -0300

    create footer component

---
+++ Front-End/web/src/components/Foother/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    height: 50px;
+    padding: 20px 0 40px;
+    margin-top: 25px;
+
+    strong {
+        font-weight: 700;
+        text-align: center;
+    
+        a {
+            color: #03be7a;
+        }
+    }
+`;
